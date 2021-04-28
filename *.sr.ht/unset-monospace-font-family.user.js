b3cb12d905da86aed0d07e4728acf873e5b733c6
Matthew Jorgensen <matthew@jrgnsn.net>
2021-04-28 11:55:13 -0500

    Add *.sr.ht/unset-monospace-font-family

---
+++ *.sr.ht/unset-monospace-font-family.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        sr.ht monospace font unset
+// @namespace   Violentmonkey Scripts
+// @match       https://*.sr.ht/*
+// @grant       none
+// @version     1.0
+// @downloadURL	https://git.sr.ht/~mjorgensen/userscripts/blob/master/*.sr.ht/unset-monospace-font-family.user.js
+// @author      Matthew Jorgensen
+// @description 4/27/2021, 9:28:13 PM
+// ==/UserScript==
+
+(function() {
+    var css = [
+	"code, kbd, pre, samp {",
+	"    font-family: monospace;",
+	"}",
+    ].join("\n");
+
+    var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+})();
