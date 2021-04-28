a7b095609aedb47bb8f5abcf37a52eaca6afe328
Matthew Jorgensen <matthew@jrgnsn.net>
2021-04-28 11:52:06 -0500

    Add retroflix.club/fix-images

---
+++ retroflix.club/fix-images.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        fix retroflix.club images
+// @namespace   Violentmonkey Scripts
+// @match       https://retroflix.club/*
+// @grant       none
+// @version     1.0
+// @author      Matthew Jorgensen
+// @downloadURL https://git.sr.ht/~mjorgensen/userscripts/tree/master/item/retroflix.club/fix-images.user.js
+// @description 4/27/2021, 9:24:18 PM
+// ==/UserScript==
+
+(function() {
+  var css = [
+    "img {",
+    "    max-width: 100%;",
+    "}",
+    "",
+    "img.movie-poster {",
+    "    max-width: unset;",
+    "}",
+    "img.bonus-formula {",
+    "    background-color: white;",
+    "}",
+  ].join("\n");
+  
+  var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+  
+  img = document.getElementsByTagName("img");
+  console.log(img)
+  for (i in img) {
+    if (img[i].height == 300) {
+      img[i].classList.add("movie-poster");
+    }
+    if (img[i].src == origin+"/pic/bonusformulac.png") {
+      img[i].classList.add("bonus-formula")
+    }
+  }
+})();
