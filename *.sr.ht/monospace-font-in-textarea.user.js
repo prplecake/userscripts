4691b66f717887d16862cea5814da09729b4522b
Matthew Jorgensen <matthew@jrgnsn.net>
2021-06-04 04:32:36 -0500

    Add monospace-font-in-textarea.user.js

---
+++ *.sr.ht/monospace-font-in-textarea.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        sr.ht monospace font in textareas
+// @namespace   Violentmonkey Scripts
+// @match       https://*.sr.ht/*
+// @grant       none
+// @version     1.0
+// @downloadURL	https://git.sr.ht/~mjorgensen/userscripts/blob/master/*.sr.ht/unset-monospace-font-family.user.js
+// @author      Matthew Jorgensen
+// ==/UserScript==
+
+(function() {
+    var css = [
+	"textarea, textarea.form-control {",
+	"    font-family: monospace;",
+	"    font-size: 11pt;",
+	"}",
+    ].join("\n");
+
+    var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+})();
