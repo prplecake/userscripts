f172609b79490e6b49fb80503f8c6692a917845b
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-02 18:17:59 -0500

    Add Misskey/click-load-more-buttons.user.js

---
+++ Misskey/click-load-more-buttons.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Misskey Autoexpander
+// @namespace   Violentmonkey Scripts
+// @match       https://mk.jrgnsn.social
+// @grant       none
+// @version     1.0
+// @author      Matthew Jorgensen
+// @downloadURL https://git.sr.ht/~mjorgensen/userscripts/blob/master/Misskey/click-load-more-buttons.user.js
+// @description Clicks "Load More" buttons inside articles
+// ==/UserScript==
+
+function clickLoadMoreButton(elementsArray) {
+	for (const [key, value] of Object.entries(elementsArray)) {
+		var buttons = value.querySelectorAll("._button");
+		buttons.forEach(item => {
+			if (item.innerHTML.includes("Load more")){
+				item.click();
+			}
+		});
+	}
+}
+setInterval(function() {
+  clickLoadMoreButton(document.getElementsByClassName("article"));
+  clickLoadMoreButton(document.getElementsByClassName("reply-to"));
+}, 2500);
