bf714ce522a1a9339b261013a1bb24b7df450706
Matthew Jorgensen <matthew@jrgnsn.net>
2022-03-23 22:27:12 -0500

    Add fix-images.user.js for help.nextcloud.com

---
+++ help.nextcloud.com/fix-images.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        fix help.nextcloud.com images
+// @namespace   Violentmonkey Scripts
+// @match       https://help.nextcloud.com/*
+// @grant       none
+// @version     1.0
+// @author      Matthew Jorgensen
+// @downloadURL https://github.com/prplecake/userscripts/blob/master/help.nextcloud.com/fix-images.user.js
+// @description 2022-03-23 22:23
+// ==/UserScript==
+
+(function() {
+	var css = [
+		"img {",
+		"    max-width: unset;",
+		"}",
+	].join("\n");
+	
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+})();
